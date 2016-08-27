854e6bf800543740750342c3eeb7805a3f5fee9b
macintorch <macintorch@gmail.com>
2016-08-27 12:22:44 +0800

    check if playerPosition more than 100, roll back

--- tutorial/src/main/java/com/ainor/JavaReview.java
+++ tutorial/src/main/java/com/ainor/JavaReview.java
@@ -7,26 +7,34 @@ public class JavaReview {
         int playerPosition = 1;
         final int BOARD_SIZE = 100;
 
-        HashMap<Integer, Integer> boardActions = new HashMap<>();
+        HashMap<Integer, Integer> snakeOrLadder = new HashMap<>();
 
-        boardActions.put(10, 15);
-        boardActions.put(20, -35);
-        boardActions.put(27, 55);
-        boardActions.put(72, -15);
-        boardActions.put(64, 5);
+        snakeOrLadder.put(10, 15);
+        snakeOrLadder.put(20, -35);
+        snakeOrLadder.put(27, 55);
+        snakeOrLadder.put(72, -15);
+        snakeOrLadder.put(64, 5);
 
 
         while (playerPosition < BOARD_SIZE) {
             int randomDice = (int) (Math.random() * 100 % 6) + 1;
             playerPosition = playerPosition + randomDice;
 
-            Integer action = boardActions.get(playerPosition);
+            Integer action = snakeOrLadder.get(playerPosition);
             if (action != null) {
                 playerPosition = playerPosition + action;
                 System.out.print("Hit an action tile! Move " + action + " steps");
             }
 
+            if (playerPosition > 100) {
+                playerPosition = 100 - (playerPosition - 100);
+                System.out.print("Final position " + playerPosition);
+            }
+
+
             System.out.println("Player rolled " + randomDice + ", Player now at " + playerPosition);
+
+
         }
     }
 }
